c62b7fd5506b3e40e45ee8418799a8d443d5000c
cauepda <cauepda@al.insper.edu.br>
2025-03-09 19:38:53 -0300

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -7,14 +7,35 @@ const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
 
+volatile bool timer_fired = false;
+volatile bool action_completed = false;
+
+alarm_id_t alarm = 0;
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    timer_fired = true;
+    return 0;
+}
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
-        if (gpio == BTN_PIN_R)
+        if (gpio == BTN_PIN_R) {
             flag_f_r = 1;
+            alarm = add_alarm_in_ms(500, alarm_callback, NULL, false);
+            if (!alarm) {
+                printf("Failed to add timer\n");
+            }
+        }
 
     } else if (events == 0x8) { // rise edge
-        if (gpio == BTN_PIN_R)
+        if (gpio == BTN_PIN_R) {
             flag_f_r = 0;
+            if (!timer_fired && alarm) {
+                cancel_alarm(alarm);
+            }
+            timer_fired = false;
+            action_completed = false;
+        }
     }
 }
 
@@ -31,9 +52,13 @@ int main() {
     gpio_set_irq_enabled_with_callback(
         BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
-    while (true) {
 
-        if (flag_f_r) {
+    while (true) {
+        if (flag_f_r && timer_fired && !action_completed) {
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+            timer_fired = false;
+            action_completed = true;
         }
+        sleep_ms(1);
     }
 }
